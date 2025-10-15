9e6d466bf0cebcc8a8cc2cd2eb11cf3ec2779557
MaXwel25 <VMVMaxim@mail.ru>
2025-10-15 18:54:56 +0300

    first individyalka

--- firstPy.py
+++ firstPy.py
@@ -3,14 +3,26 @@ str1 = "fhb5kbfыshfm."
 # str1 = input()
 if not str1.endswith("."):
     print("Error: zero not found!")
+    exit()
 else:
     str = str1.split(".")[0]
 
-    count = {}
-    for char in str:
-        if "a" <= char <= "z":
-            count[char] = count.get(char, 0) + 1
+alfavit = "abcdefghijklmnopqrstuvwxyz"
+let = ""
+for char in str1:
+    if char in alfavit:
+        let += char
 
-    for sps in sorted(count.keys()):
-        print(sps, count[sps])
+sort_let = "".join(sorted(let))
+if sort_let:
+    cur_let = sort_let[0]
+    count = 1
 
+    for i in range(1, len(sort_let)):
+        if sort_let[i] == cur_let:
+            count += 1
+        else:
+            print(f"{cur_let}{count}")
+            cur_let = sort_let[i]
+            count = 1
+    print(f"{cur_let}{count}")
